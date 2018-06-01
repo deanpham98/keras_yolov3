908bfb3743011deb3b6d11827e053f000def3261
Khoi Pham <khoipham2016@gmail.com>
2018-06-01 15:31:54 +0800

    add scripts to get data

---
+++ Scripts/get_voc_dataset.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Go to data directory
+cd ../data/
+
+# Download data
+wget https://pjreddie.com/media/files/VOCtrainval_11-May-2012.tar
+wget https://pjreddie.com/media/files/VOCtrainval_06-Nov-2007.tar
+wget https://pjreddie.com/media/files/VOCtest_06-Nov-2007.tar
+
+# Extract data into subdirectory VOCdevkit/
+tar xf VOCtrainval_11-May-2012.tar
+tar xf VOCtrainval_06-Nov-2007.tar
+tar xf VOCtest_06-Nov-2007.tar
+
+# Rename subdirectory VOCdevkit/ to VOC/
+mv VOCdevkit/ VOC/
+
+
+
+
