1d750c3ae061efb45986f86068ecbaece163f33c
Khoi Pham <khoipham2016@gmail.com>
2018-06-05 04:52:58 +0800

    Fix docker_run shell script

--- Scripts/docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-xhost +local:docker
-YOLOv3="$(cd ../ ; pwd -P)"
-sudo nvidia-docker run --rm -ti -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY -v $YOLOv3:/yolov3 -p 8000:8080 nguyenkh001/vebits-yolov3:latest
-xhost -local:docker
