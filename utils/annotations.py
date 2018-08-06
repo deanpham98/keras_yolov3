a08da45e68668cf4a041b36a81da9cbc3944d934
Pham Nguyen Khoi <34411855+deanpham98@users.noreply.github.com>
2018-08-06 17:29:20 +0800

    Update annotations.py

--- utils/annotations.py
+++ utils/annotations.py
@@ -2,34 +2,46 @@ import os
 import sys
 import pdb
 
+import numpy as np 
+import pandas as pd 
 
-import numpy as np
-import cv2
 
-repo_dir = os.path.join(os.path.dirname(__file__), '..')
-coco_dir = os.path.join(repo_dir, 'data', 'yicam')
-dataset = ('data_cocoformat',)
+# def calc_dividend_value(row):
+# 	try:
+# 		return row['Dividend Value'] + dividend_info[row['Date']]
 
-# Write out annotations
-for data in dataset:
-        with open(os.path.join(repo_dir, 'data', 'annotations', data + '.txt'), 'a') as file:
-                root = os.path.join(coco_dir, data, 'labels')
-                listdir = os.listdir(root)
-                for filename in listdir:
-                        with open(os.path.join(root, filename), 'r') as annot:
-                                image_path = os.path.join('yicam', data, 'images', filename)[:-4] + '.jpg'
-                                boxes = ''
-                                img = cv2.imread(os.path.join(repo_dir, 'data', image_path))
-                                size = np.array(img.shape[0:2])
-                                for line in annot.readlines():
-                                        box = [i.strip() for i in line.split()]
-                                        b = np.array([float(i) for i in box[1:]])
-                                        b[0:2] = (b[0:2] - b[2:4] / 2)
-                                        b[2:4] = b[0:2] + b[2:4]
-                                        b[0:2] *= size[::-1]
-                                        b[2:4] *= size[::-1]
-                                        box[1:] = [str(i) for i in b.astype('int32')]
-                                        boxes = boxes + ' ' + ','.join(box[1:]) + ',' + box[0]
-                                if boxes != '':
-                                        file.write(image_path + boxes + '\n')
+# def calc_adjusted_dividend():
 
+
+# def calc_reinvested_dividend():
+
+
+# def calc_total_value():
+
+
+
+
+if __name__ == '__main__':
+	price_records = pd.read_excel('price_records_1.xlsx').set_index('Date')
+	price_records = price_records.iloc[::-1]
+
+	dividend_info = pd.read_excel('dividend_info_1.xlsx')
+	dividend_info = dividend_info.drop(labels=['Distribution Type', 'Currency', 'Income Dividend Taxation'], axis=1)
+	dividend_info = dividend_info.iloc[::-1]
+	dividend_info['Ex-Date'] = dividend_info['Ex-Date'].apply(lambda x: pd.Timestamp.strptime(x, '%d-%b-%Y'))
+	dividend_info['Payment Date'] = dividend_info['Payment Date'].apply(lambda x: pd.Timestamp.strptime(x, '%d-%b-%Y') if x == x else x)
+	dividend_info = dividend_info[(dividend_info['Payment Date'].isnull() | ((dividend_info['Payment Date'] < price_records.index[-1]) & (dividend_info['Ex-Date'] > price_records.index[0])))]
+	
+	price_records['Dividend Value'] = np.nan
+	null_info = dividend_info[dividend_info['Payment Date'].isnull()]
+	price_records['Dividend Value'].loc[null_info['Ex-Date']] = null_info['Value'].tolist()
+	pdb.set_trace()
+	complete_info = dividend_info[dividend_info['Payment Date'].notnull()]
+	price_records['Dividend Value'].loc[complete_info['Payment Date']] = complete_info['Value'].tolist()
+
+
+	fill_info = dividend_info.bfill(axis=0).loc[null_info.index]
+	fill_info = fill_info.groupby(['Payment Date'])['Value'].sum()
+	price_records['Dividend Value'].loc[fill_info.index] += fill_info.tolist()
+
+	print('price_records: ', price_records[price_records['Dividend Value'].notnull()])
